74d6e7e4b34ee892c45bef2664a55127be371050
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-02-05 18:18:09 +0530

    footer added

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <div className="footer">
+            <h2 className="footer-text">Made with ❤️ by <a href ="https://github.com/GaganChaudhary6378">Gagan Chaudhary</a></h2>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Footer from './Footer'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
   <App />
+  <Footer/>
+  </div>
 )
